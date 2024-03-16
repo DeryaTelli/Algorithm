7ebbbe838c9ba178759afe32bad3aa9632db2580
Mobilyst <2002derya2002@gmail.com>
2024-03-16 17:27:08 +0300

    add comment

--- InsertionSortAlgorithm/src/Main.java
+++ InsertionSortAlgorithm/src/Main.java
@@ -8,13 +8,19 @@ public class Main {
         }
     }
     static void insertionSort(int[] A, int n) {
+        // once kodu for dongusu ile dolasiyoruz
         for(int j=1 ; j<n; j++){
+            // anahtarima a nin dizi elemanlarini atiyorum tek tek
             int key=A[j];
+            // j degerini 1 azaltip i ye esitliyorum
             int i=j-1;
+            //while dongusunde kotrol ediyorum
             while(i>=0 && A[i]>key){
+                //kiyaslama yaptiktan sonra adizimdeki i index degerini yeni yerine atiyorum i yi bir azaltiyorum
                 A[i+1]=A[i];
                 i-=1;
             }
+            // anahtar degerimi yeni degerine atiyorum ve dongu kucukten buyuge dogru siralanincaya kadar donguyu devam ettiyorum
             A[i+1]=key;
         }
     }
